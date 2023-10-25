fc1432f925ffed960a3323c1f11ec6520cc22827
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-10-25 13:22:07 -0500

    changed port

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./utils/app.js";
 import { connectToDatabase } from "./db/connection.js";
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 4000;
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => console.log("Server Open AND Database Connected"));
