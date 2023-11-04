11cd8725c5f0ca5eb7c05d00078058bad8259dc6
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-03 19:08:51 -0500

    add browser routing

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material'
+import { BrowserRouter } from "react-router-dom";
 
 const theme = createTheme({
   typography: {
@@ -13,8 +14,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
