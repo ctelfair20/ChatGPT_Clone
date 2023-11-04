26389c665c923f00cd46e0bfe2eabb7c0440e560
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-03 19:31:21 -0500

    added routes for all pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Chats from "./pages/Chats"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import NotFound from "./pages/NotFound"
+
 function App() {
 
   return (
-    <main>hello world</main>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/notFound" element={<NotFound />} />
+      </Routes>
+    </main>
   )
 }
 
