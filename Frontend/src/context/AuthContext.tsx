70cad67ac78d57f5c87f12b84da68729b699fb0c
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-10 10:07:46 -0600

    added to userAuthContext

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -1,16 +1,37 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
 
 type User = {
   name: string
   email: string
-}
+};
 
 type UserAuth = {
-  isLoggedIn: boolean,
-  user: User | null,
+  isLoggedIn: boolean
+  user: User | null
   login: (email: string, password: string) => Promise<void>
   signup: (name: string, email: string, password: string) => Promise<void>
   logout: () => Promise<void>
-}
+};
 
-export const AuthContext = createContext<UserAuth | null>(null);
+const AuthContext = createContext<UserAuth | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    // check of user is signed in
+  }, []);
+
+  const login = async (email: string, password: string) => { }
+  const signup = async (name: string, email: string, password: string) => { }
+  const logout = async () => { }
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, user, login, signup, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+};
+
+export const useAuthContext = () => useContext(AuthContext);
