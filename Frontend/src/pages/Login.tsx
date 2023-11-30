3e2c6e48bf030647c3af36d53c6e6baa985dd58e
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-30 12:01:07 -0600

    added robot image to the login page with styles

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,6 +1,14 @@
+import { Box } from "@mui/material"
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <Box width={'100%'} height={'100%'} display='flex' flex={1} style={{ background: 'red' }}
+    >
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", sx: "none" }}>
+        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box></Box>
+    </Box>
   )
 }
 
