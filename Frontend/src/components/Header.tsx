30b4757bcdd6ff28647460fe77d906ca8270e534
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-29 21:03:23 -0600

    added nav comp to header comp

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,12 +1,30 @@
 import { AppBar, Toolbar } from "@mui/material"
+import { useAuthContext } from "../context/AuthContext";
 import Logo from "./shared/Logo"
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuthContext();
+
   return (
     <AppBar sx={{ bgcolor: "transparent", boxShadow: "none", position: "static", }}>
       {/* this toolbar seems is flexed by defualt */}
       <Toolbar>
         <Logo></Logo>
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink to="/chats" text="Chat List" textColor="black" bg="#00fffc" />
+              <NavigationLink to="/" text="Logout" textColor="white" bg="#51538f" onClick={auth?.logout} />
+            </>
+          ) : (
+            <>
+              {/* onClick={auth?.login}...onClick={auth?.signup} */}
+              <NavigationLink to="/login" text="Login" textColor="black" bg="#00fffc" />
+              <NavigationLink to="/signup" text="Signup" textColor="white" bg="#51538f" />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   )
