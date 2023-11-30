27592f5b17742c86a93a3e00cf3407b0498005ee
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-30 14:48:23 -0600

    clean up login page

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -2,12 +2,34 @@ import { Box } from "@mui/material"
 
 const Login = () => {
   return (
-    <Box width={'100%'} height={'100%'} display='flex' flex={1} style={{ background: 'red' }}
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      flex={1}
+      style={{ background: 'red' }}
     >
-      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", sx: "none" }}>
+      RED
+      <Box
+        padding={8}
+        mt={8}
+        display={{ md: "flex", sm: "none", sx: "none" }}
+      >
         <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+        <Box style={{ background: 'green' }}>Flexing?</Box>
       </Box>
-      <Box></Box>
+
+      <Box
+        display="flex"
+        flex={{ sx: 1, md: 0.5 }}
+        justifyContent="center"
+        alignItems="center"
+        padding={2}
+        ml="auto"
+        mt={16}
+        style={{ background: "blue" }}
+      >
+      </Box>
     </Box>
   )
 }
