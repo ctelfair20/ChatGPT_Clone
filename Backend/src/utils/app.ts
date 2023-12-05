f2d5c526d589728fee1906d68252818bc793960a
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-12-04 22:08:04 -0600

    took care of cors issue

--- Backend/src/utils/app.ts
+++ Backend/src/utils/app.ts
@@ -3,15 +3,16 @@ import morgan from "morgan";
 import appRouter from "../routes/index.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 //connection to the mongodb database using environment file
 config();
-
 const app = express();
-app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove in production
 app.use(morgan("dev"));
 
