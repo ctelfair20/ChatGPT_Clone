f5aff291bc1b96c83dd4b87140399b2332f0d673
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-12-28 14:17:27 -0600

    start styling chat page

--- Frontend/src/pages/Chats.tsx
+++ Frontend/src/pages/Chats.tsx
@@ -1,6 +1,16 @@
+import { Avatar, Box } from "@mui/material"
+
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <Box sx={{ display: "flex", width: "100%", height: "100%", mt: 3, gap: 3, background: "red" }}>parent
+      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" }, background: "blue" }}>
+        child
+        <Box sx={{ display: "flex", width: "100%", height: "60vh", bgcolor: "rgb(17,29,39)", borderRadius: 5, flexDirection: "column", mx: 3 }}>
+          grand child
+          <Avatar></Avatar>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
