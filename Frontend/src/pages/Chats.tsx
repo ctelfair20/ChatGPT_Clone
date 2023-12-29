80fd74afc80ffc04bbc28b6b1f60f0b9c3c3dfa5
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-12-28 22:19:21 -0600

    removed colors

--- Frontend/src/pages/Chats.tsx
+++ Frontend/src/pages/Chats.tsx
@@ -6,11 +6,9 @@ const Chats = () => {
   const auth = useAuthContext();
 
   return (
-    <Box sx={{ display: "flex", width: "100%", height: "100%", mt: 3, gap: 3, background: "red" }}>parent
-      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" }, background: "blue" }}>
-        child
+    <Box sx={{ display: "flex", width: "100%", height: "100%", mt: 3, gap: 3 }}>
+      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" } }}>
         <Box sx={{ display: "flex", width: "100%", height: "60vh", bgcolor: "rgb(17,29,39)", borderRadius: 5, flexDirection: "column", mx: 3 }}>
-          grand child
           <Avatar sx={{ mx: "auto", my: 2, bgcolor: "white", color: "black", fontWeight: 700, }}
           >
             {auth?.user?.name[0]}
