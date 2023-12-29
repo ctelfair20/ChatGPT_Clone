c7bab7f5ccadd5c25f69176b812f0b658639861e
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-12-29 15:44:40 -0600

    added placeholder chats

--- Frontend/src/pages/Chats.tsx
+++ Frontend/src/pages/Chats.tsx
@@ -2,6 +2,19 @@ import { Avatar, Box, Button, Typography } from "@mui/material"
 import { useAuthContext } from "../context/AuthContext"
 import { red } from "@mui/material/colors";
 
+const chatsMessages = [
+  { "role": "user", "content": "Hello, AI assistant!" },
+  { "role": "assistant", "content": "Hi there! How can I help you today?" },
+  { "role": "user", "content": "I need information about weather forecasts." },
+  { "role": "assistant", "content": "Sure! In which location would you like to know the weather?" },
+  { "role": "user", "content": "Provide the weather forecast for New York." },
+  { "role": "assistant", "content": "The weather in New York is currently 72°F with clear skies." },
+  { "role": "user", "content": "What's on my calendar for today?" },
+  { "role": "assistant", "content": "Let me check. You have a meeting at 2:00 PM and a deadline at 5:00 PM." },
+  { "role": "user", "content": "Set a reminder for my meeting." },
+  { "role": "assistant", "content": "Reminder set for your meeting at 2:00 PM." },
+]
+
 const Chats = () => {
   const auth = useAuthContext();
 
@@ -37,6 +50,7 @@ const Chats = () => {
         </Typography>
         <Box sx={{ width: "100%", height: "60vh", borderRadius: 3, mx: "auto", display: "flex", flexDirection: "column", overflow: "scroll", overflowX: "hidden", overflowY: "auto", scrollBehavior: "smooth" }}
         >
+          {chatsMessages.map((chat) => <div>{chat.content}</div>)}
         </Box>
       </Box>
     </Box>
